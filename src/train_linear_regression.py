f7b5a5bee3a3a57c10f75b7ac8561bd92ba775ee
nastyaalmaeva <nastyaalmaeva@mail.ru>
2026-06-23 23:26:07 +0300

    feat: train linear regression model

--- src/train_linear_regression.py
+++ src/train_linear_regression.py
@@ -5,20 +5,20 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 import os
 
-os.makedirs('models', exist_ok=True)
+os.makedirs('../models', exist_ok=True)
 
 # загрузка выборок из data/processed/
-X_train = pd.read_csv('data/processed/X_train.csv')
-X_test = pd.read_csv('data/processed/X_test.csv')
-y_train = pd.read_csv('data/processed/y_train.csv')
-y_test = pd.read_csv('data/processed/y_test.csv')
+X_train = pd.read_csv('../data/processed/X_train.csv')
+X_test = pd.read_csv('../data/processed/X_test.csv')
+y_train = pd.read_csv('../data/processed/y_train.csv')
+y_test = pd.read_csv('../data/processed/y_test.csv')
 
 # обучение
 model = LinearRegression()
 model.fit(X_train, y_train)
 
 # сохранение модели
-joblib.dump(model, 'models/linear_regression_model.pkl')
+joblib.dump(model, '../models/linear_regression_model.pkl')
 print('модель сохранена: models/linear_regression_model.pkl')
 
 # прогноз на тесте
