433558704731dfb63205af68eed2c6f9453da2a4
nastyaalmaeva <nastyaalmaeva@mail.ru>
2026-06-23 23:26:40 +0300

    feat: train XGBoost model

--- src/train_xgboost.py
+++ src/train_xgboost.py
@@ -5,12 +5,12 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 import os
 
-os.makedirs('models', exist_ok=True)
+os.makedirs('../models', exist_ok=True)
 
-X_train = pd.read_csv('data/processed/X_train.csv')
-X_test = pd.read_csv('data/processed/X_test.csv')
-y_train = pd.read_csv('data/processed/y_train.csv')
-y_test = pd.read_csv('data/processed/y_test.csv')
+X_train = pd.read_csv('../data/processed/X_train.csv')
+X_test = pd.read_csv('../data/processed/X_test.csv')
+y_train = pd.read_csv('../data/processed/y_train.csv')
+y_test = pd.read_csv('../data/processed/y_test.csv')
 
 model = xgb.XGBRegressor(
     n_estimators=100,
@@ -24,7 +24,7 @@ model = xgb.XGBRegressor(
 )
 model.fit(X_train, y_train)
 
-joblib.dump(model, 'models/xgboost_model.pkl')
+joblib.dump(model, '../models/xgboost_model.pkl')
 print('модель сохранена: models/xgboost_model.pkl')
 
 y_pred = model.predict(X_test)
